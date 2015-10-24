0328702664a34f3668ef6de5eea11b463ca63af1
Syed Mohammad Ibrahim <sumiibi@gmail.com>
2015-10-24 15:00:03 +0530

    Vigenere Ciphere for encryption or decryption

--- VigenereCipher.py
+++ VigenereCipher.py
@@ -1,5 +1,5 @@
-ALPHABETS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
+#Created a main function under the __main__ namespace. So whenever the global namespace is called, the main
+#function gets invoked
 def main():
     print ("\t\tVigener Cipher in python")
     
@@ -11,8 +11,13 @@ def main():
         print ("Message Decrypted: ", ConvertMessage(descision, message, key))
     elif (descision[0] == 'e' or descision[0] == 'E'):
         print ("Message Encrypted: ", ConvertMessage(descision, message, key))
-        
+
+#Descision taking function        
 def getDescision():
+    '''
+    # Get the descision of the user.
+    # Encrypt the message or Decrypt it.
+    '''
     while (True):
         print ("What do you want to do? (Encrypt/Decrypt): ",end='')
         desc = input()
@@ -21,12 +26,20 @@ def getDescision():
         else:
             print ("Wrong Input. Please input correctly")
                 
+#Message input function
 def getMessage():
+    '''
+    # Take message as input
+    '''
     print ("Enter a message: ", end='')
     mssg = input()
     return mssg
     
+#Key input function
 def getKey():
+    '''
+    # Input the key from the user and check whether the key contains only alphabets or not.
+    '''
     flag = 0
     while (True):
         print ("Enter a key string: ", end='')
@@ -41,22 +54,26 @@ def getKey():
 
         if (flag == 0):
             return key
-    
+
+#Convert the message according to the descision provided
 def ConvertMessage(descision, message, key):
+    '''
+    # This function converts the message according to the descision provided by the user and stores it in the final string
+    '''
     Final = ''
-    keyIndex = 0
+    keyIndex = 0        #Index of the alphabets from ALPHABETS
     key = key.upper()
     len_key = len(key)
     
     for letter in message:
         num = ALPHABETS.find(letter.upper())
-        if (letter.isalpha() and (num != -1)):
+        if (letter.isalpha() and (num != -1)):              #if the given letter is an alphabet
             if (descision[0] == 'e' or descision == 'E'):
                 num += ALPHABETS.find(key[keyIndex])
             elif (descision[0] == 'd' or descision == 'D'):
                 num -= ALPHABETS.find(key[keyIndex])
         
-            num = num % 26
+            num = num % 26                                  #Wrap the key around
         
             if (letter.isupper()):
                 Final = Final + str(ALPHABETS[num])
@@ -64,7 +81,7 @@ def ConvertMessage(descision, message, key):
                 Final = Final + str(ALPHABETS[num].lower())
             
             keyIndex = keyIndex + 1
-            if (keyIndex >= len_key):
+            if (keyIndex >= len_key):                       #if the Index value exceeds the given key length, reset it
                 keyIndex = 0
                 
         else:
@@ -72,5 +89,7 @@ def ConvertMessage(descision, message, key):
     
     return Final
     
+ALPHABETS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+#calling the main function from the __main__ namespace
 if (__name__ == '__main__'):
     main()
