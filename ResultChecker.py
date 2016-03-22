d29f27448808de926e4120611447f6495166906f
Syed Mohammad Ibrahim <sumiibi@gmail.com>
2016-03-22 22:46:22 +0530

    This script checks for RGPV result

---
+++ ResultChecker.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import urllib.request, os, re, time, urllib.error
+
+while True:
+    try:
+        html_dump = str(urllib.request.urlopen('https://www.rgpv.ac.in/campus/Result.htm').read())
+        string_match = re.findall('B.E. V Semester'.strip(), html_dump)
+        if len(string_match) == 0:
+            os.system("notify-send 'Enjoy!, No need to worry' 'Result is not declared yet'")
+            time.sleep(900)
+        else:
+            os.system("notify-send 'Shit just got real!' 'Result Declared'")
+            quit()
+    except urllib.error.URLError:
+        os.system ("notify-send 'Network Error!!' 'Unable to connect'")
+        quit()
